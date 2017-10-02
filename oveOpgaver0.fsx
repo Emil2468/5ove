91ceb06f329ad7c761008b3582a68b91f2e1f082
Emil2468 <emh@calaha.dk>
2017-10-02 11:37:43 +0200

    Completed 5ø1

--- oveOpgaver0.fsx
+++ oveOpgaver0.fsx
@@ -1,4 +1,16 @@
-let oneToN (n:int) : int list = 
+let oneToN (n:int) : int list =
     [1 .. n]
 
-printfn "%A" (oneToN 10)
+let lst = oneToN 15
+
+let multiplicity (x:int) (xs:int list) : int =
+    let mutable n = 0
+    let len = xs.Length
+    let mutable i = 0
+    while i < len do
+        if (xs.[i] = x) then
+            n <- n + 1
+        i <- i + 1
+    n
+
+printfn "%A" (multiplicity 19 [10; 2; 3; 10; 11; 10])
