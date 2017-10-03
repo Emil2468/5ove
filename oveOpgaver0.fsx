325fd4844ff93e37ec617dffa44503ae744f0c8b
Emil2468 <emh@calaha.dk>
2017-10-03 22:16:42 +0200

    Added opgavenumre

--- oveOpgaver0.fsx
+++ oveOpgaver0.fsx
@@ -1,8 +1,12 @@
+printfn "5.0"
+
 let oneToN (n:int) : int list =
     [1 .. n]
 
 let lst = oneToN 15
+printfn "%A\n" lst
 
+printfn "5.1"
 let multiplicity (x:int) (xs:int list) : int =
     let mutable n = 0
     let len = xs.Length
@@ -13,31 +17,35 @@ let multiplicity (x:int) (xs:int list) : int =
         i <- i + 1
     n
 
-printfn "%A" (multiplicity 19 [10; 2; 3; 10; 11; 10])
-
+printfn "%A\n" (multiplicity 19 [10; 2; 3; 10; 11; 10])
+printfn "5.2"
 let split (xs:int list) : (int list * int list) =
     let x1 = List.init (xs.Length / 2 + xs.Length % 2) (fun x -> xs.[x * 2])
     let x2 = List.init (xs.Length / 2) (fun x -> xs.[x * 2 + 1])
     (x1, x2)
 
-printfn "%A" (split (oneToN 10))
+printfn "%A\n" (split (oneToN 10))
 
+printfn "5.3"
 let reverseApply x f =
     f x
 
-printfn "%A" (reverseApply lst split)
+printfn "%A\n" (reverseApply lst split)
 
+//5.4
 let func1 (n : int) =
     oneToN
 
 let func2 (f) : int =
     f 19 [10; 2; 3; 10; 11; 10]
 
+printfn "5.5"
 let evens (lst : int list) : int list =
     List.filter (fun x -> x % 2 = 0) lst
 
-printfn "%A" (evens lst)
+printfn "%A\n" (evens lst)
 
+printfn "5.6"
 let add2 x =
   x + 2
 
@@ -48,9 +56,10 @@ let sub2 x =
 let applyList (lst : ('a -> 'b) list) (n : 'a) : 'b list =
   List.map (reverseApply n) lst
 
-printfn "%A" (applyList [sub2; add2] 18)
+printfn "%A\n" (applyList [sub2; add2] 18)
 
+printfn "5.8"
 let ssort (xs : int list) : int list =
   Set.toList (Set.ofList xs)
 
-printfn "%A" (ssort [4; 3; 2; 6; 1; 8; 2])
+printfn "%A\n" (ssort [4; 3; 2; 6; 1; 8; 2])
